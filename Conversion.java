2795b8ef422d5acbb6219d3774056c55bb545e39
Adam O'Brien <adam.patrick.obrien@gmail.com>
2018-05-20 10:41:12 +0100

    Created basic user interface with Swing

--- Conversion.java
+++ Conversion.java
@@ -1,5 +1,37 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
 public class Conversion {
     public static void main(String[] args) {
-        System.out.println("Conversion");
+        JFrame f = new JFrame();
+
+        final JLabel l1 = new JLabel("Input Field 1");
+        l1.setBounds(50, 5, 120, 20);
+        final JTextField tf1 = new JTextField();
+        tf1.setBounds(50, 25, 120, 20);
+
+        final JLabel l2 = new JLabel("Input Field 2");
+        l2.setBounds(50, 45, 120, 20);
+        final JTextField tf2 = new JTextField();
+        tf2.setBounds(50, 65, 120, 20);
+
+        JButton b = new JButton("Convert");
+        b.setBounds(50, 90, 120, 30);
+        b.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(tf1.getText());
+                System.out.println(tf2.getText());
+                System.exit(0);
+            }
+        });
+
+        f.add(b);
+        f.add(l1);      f.add(l2);
+        f.add(tf1);     f.add(tf2);
+        f.setSize(400, 400);
+        f.setLayout(null);
+        f.setVisible(true);
     }
 }
