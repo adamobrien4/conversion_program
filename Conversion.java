ce2b7a7bd4d8c20ede823e1e25da55d5a752c19a
Adam O'Brien <adam.patrick.obrien@gmail.com>
2018-05-20 11:25:30 +0100

    Dropdown menu test

--- Conversion.java
+++ Conversion.java
@@ -7,7 +7,38 @@ public class Conversion {
     public static void main(String[] args) {
         JFrame f = new JFrame();
 
-        final JLabel l1 = new JLabel("Input Field 1");
+        String[] types = {
+            "Speed",
+            "Volume",
+            "Currency"
+        };
+
+        final JComboBox cb = new JComboBox(types);
+        cb.setBounds(50, 100, 90, 20);
+
+        final JButton b = new JButton("Click Me");
+        b.setBounds(50, 150, 90, 20);
+        b.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(types[cb.getSelectedIndex()]);
+                System.exit(0);
+            }
+        });
+
+        final JButton sb = new JButton(new ImageIcon("C:\\Users\\Adam\\Pictures\\Swap.png"));
+        sb.setBounds(50, 200, 120, 90);
+
+        f.add(cb);
+        f.add(b); f.add(sb);
+
+
+
+
+
+
+
+
+        /*final JLabel l1 = new JLabel("Input Field 1");
         l1.setBounds(50, 5, 120, 20);
         final JTextField tf1 = new JTextField();
         tf1.setBounds(50, 25, 120, 20);
@@ -29,7 +60,7 @@ public class Conversion {
 
         f.add(b);
         f.add(l1);      f.add(l2);
-        f.add(tf1);     f.add(tf2);
+        f.add(tf1);     f.add(tf2);*/
         f.setSize(400, 400);
         f.setLayout(null);
         f.setVisible(true);
